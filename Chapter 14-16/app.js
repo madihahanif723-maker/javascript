bc2e467e0e4fc9312fc764f0b9dc7e7dc1f657aa
madiha <madihahanif723>
2026-01-05 13:50:56 +0500

    14-19

---
+++ Chapter 14-16/app.js
@@ -0,0 +1,33 @@
+//.............question:1..............//
+
+var students = [];
+
+//.............question:2...............//
+
+var students = new Array();
+
+//............question:3..............//
+
+var cities = ["Karachi", "Lahore", "Faisalabad", "Hyderabad"];
+document.write("String Array : " + cities + "<br/>" + "<br/>");
+
+//............question:4..............//
+
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46]
+document.write("Number Array : " + num + "<br/>" + "<br/>")
+
+//............question:5..............//
+
+var Boo = [true, false, true, false];
+document.write("Boolean Array: " + Boo + "<br/>" + "<br/>");
+
+//............question:6..............//
+
+var array = ["Madiha", "Hanif", 7, true, false];
+document.write("Mixed Array: " + array + "<br/>" + "<br/>");
+
+//............question:7..............//
+
+
+var qualif = ["SSC" + "<br/>" + "HSC" + "<br/>" + "BCS" + "<br/>" + "BS" + "<br/>" + "BCOM" + "<br/>" + "MS" + "<br/>" + "M.Phil" + "<br/>" + "PHD"+ "<br/>"];
+document.write(qualif + "<br/>")
