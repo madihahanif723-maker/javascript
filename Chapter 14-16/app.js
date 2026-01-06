c77b1d539c46aa3e348fb2311266e7b600bca6cf
madiha <madihahanif723>
2026-01-07 02:11:42 +0500

    12-14

--- Chapter 14-16/app.js
+++ Chapter 14-16/app.js
@@ -29,5 +29,63 @@ document.write("Mixed Array: " + array + "<br/>" + "<br/>");
 //............question:7..............//
 
 
-var qualif = ["SSC" + "<br/>" + "HSC" + "<br/>" + "BCS" + "<br/>" + "BS" + "<br/>" + "BCOM" + "<br/>" + "MS" + "<br/>" + "M.Phil" + "<br/>" + "PHD"+ "<br/>"];
+var qualif = ["SSC" + "<br/>" + "HSC" + "<br/>" + "BCS" + "<br/>" + "BS" + "<br/>" + "BCOM" + "<br/>" + "MS" + "<br/>" + "M.Phil" + "<br/>" + "PHD" + "<br/>"];
 document.write(qualif + "<br/>")
+
+//............question:9..............//
+
+var colos = ["red", "yellow", "brown"];
+document.write(colos + "<br>");
+
+var colour = prompt("Enter color to add beginning");
+colos.unshift(colour);
+console.log(colos + "<br>");
+
+var endcolor = prompt("Enter color to add Ending");
+colos.push(endcolor + "<br>");
+console.log(colos + "<br>");
+
+var startcolor = prompt("Enter color to add beginning");
+colos.unshift(startcolor);
+console.log(colos + "<br>");
+
+colos.shift();
+console.log(colos + "<br>");
+
+colos.pop();
+console.log(colos + "<br>");
+
+var index = +prompt("which index you like to add color");
+var newcolor = prompt("which color you like to add that index");
+colos.splice(index, 0, newcolor);
+document.write(colos + "<br>");
+
+let indexToDelete = prompt("Enter the index to start deleting colors from:");
+let countToDelete = prompt("Enter the number of colors to delete:");
+colors.splice(indexToDelete, countToDelete);
+document.write(colos + "<br>");
+
+//............question:10..............//
+
+var scores = [320, 230, 480, 120];
+document.write("Scores of Students :" + scores + "<br>");
+
+scores.sort(function (a, b) {
+    return a - b;});
+
+document.write("Ordered Scores of Students :" + scores);
+
+//............question:11..............//
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+document.write(cities + "<br>");
+document.write(cities.slice(2, 4));
+
+//............question:12..............//
+
+var arr = ["This ", " is ", " my ", " cat"];
+// arry
+document.write(arr + "<br>");
+//string
+var result = arr.join("");
+document.write(result);
