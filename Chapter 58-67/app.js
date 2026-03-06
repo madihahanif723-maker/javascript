4f7c08d47232ae733868e9807caf573f398095b8
madihahanif723 <madihahanif723@gmail.com>
2026-03-06 13:47:45 +0500

    assignment 53-67

--- Chapter 58-67/app.js
+++ Chapter 58-67/app.js
@@ -1,29 +1,54 @@
+//...question 1.....//
 // i
-var x = document.getElementById("main-content");
+var main = document.getElementById("main-content");
+// ii
+console.log = (main.childNodes);
+// iii
+var render = document.getElementsByClassName("render");
+for (var i = 0; i < render.length; i++) {
+    document.write(render[i].innerHTML + "<br>");
+    
+}
+// iv
+document.getElementById("first-name").value = "ALex";
+document.getElementById("last-name").value = "Smith";
+document.getElementById("email").value = "alexbank@wexample.com";
 
+
+//...question 2.....//
+// i
+var a = document.getElementById("form-content");
+// console.log(a.nodeType);
 
 // ii
-var y = x.children;
-
-for(var i=0; i<y.length; i++){
-document.write(y[i].innerHTML);
-document.write("<br>");
-}
-
+var b = document.getElementById("last-name");
+// console.log(b.nodeType);
 
 // iii
-var z = document.getElementsByClassName("render");
-
-for(var i=0; i<z.length; i++){
-document.write(z[i].innerHTML);
-document.write("<br>");
-}
-
+var c = document.getElementById("last-name");
+// console.log(c.childNodes[0].nodeType);
 
 // iv
-document.getElementById("first-name").value = "Alex";
+var d = document.getElementById("main-content");
+// console.log(d.firstChild);
+document.write(d.firstChild)
+document.write(d.lastChild)
 
 
-// v
-document.getElementById("last-name").value = "Bank";
-document.getElementById("email").value = "alexbank@example.com";
+
+// vi
+// var f = document.getElementById("last-name");
+// // console.log(f.nextSibling);
+// document.write(f.nextSibling)
+// document.write(f.previousSibling)
+
+// viii
+var h = document.getElementById("email");
+// console.log(h.parentNode);
+document.write(h.parentNode)
+// console.log(h.nodeType);
+// document.write(h.nodeType)
+
+
+
+
